77ef1d080b1df5282e2cc8688347f138139e2372
dpmin <dpmin7@gmail.com>
2019-12-25 13:43:29 +0900

    leetcode: 55 Jump Game - Optimize to O(N)
    https://leetcode.com/problems/perfect-squares/

--- 3_leetcode.com/dpmin/55.cpp
+++ 3_leetcode.com/dpmin/55.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {        
-        bool *avail = new bool[nums.size()];
-        for (int i = 0; i < nums.size(); i++)
-            avail[i] = false;
-        
-        avail[0] = true;
-        for (int i = 0; i < nums.size(); i++) {            
-            if (avail[i]) {
-                if (i+nums[i] >= nums.size()-1) {
-                    avail[nums.size()-1] = true;
-                    break;
-                } else {
-                    for (int j = i+1; j <= i+nums[i]; j++)
-                        avail[j] = true;
-                }
-            }
-        }
-        
-        bool ret = avail[nums.size()-1];
-        delete []avail;        
-        return ret;
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.size() && i <= maxReach; i++)
+            maxReach = max(maxReach, i+nums[i]);
+
+        return maxReach >= nums.size()-1;
     }
 };
